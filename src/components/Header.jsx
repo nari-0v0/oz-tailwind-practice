33c97f850785386ac329c0b311a1fd8a327c8ad5
nari-0v0 <ej9811@naver.com>
2025-07-11 15:38:11 +0900

    css적용

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,20 @@
-export default function Header() {
+function Header() {
   return (
-    <header className="header-container">
-      <h2>OZ코딩스쿨</h2>
-      <ul className="menu-container">
-        <li>로그인</li>
-        <li>회원가입</li>
-        <li>내클래스</li>
+    <header className="header-container w-full flex items-center justify-between py-4 border-b-2 border-gray-300 bg-[#121212]">
+      <h2 className="text-2xl font-bold text-gray-200">OZ코딩스쿨</h2>
+      <ul className="menu-container flex gap-4 text-sm font-semibold">
+        <li>
+          <a href="#">로그인</a>
+        </li>
+        <li>
+          <a href="#">회원가입</a>
+        </li>
+        <li>
+          <a href="#">내클래스</a>
+        </li>
       </ul>
     </header>
   );
 }
+
+export default Header;
